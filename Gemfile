bb02fa8c5fc007419a565940d000f97feb0082d2
kret <andreskee@gmail.com>
2011-06-28 21:37:13 +0200

    Initial Rails project commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+gem 'sqlite3'
+gem 'rmagick'
+gem 'carrierwave'
+gem 'rails3-jquery-autocomplete', :git => 'git://github.com/kret/rails3-jquery-autocomplete.git'
+gem 'jquery-rails'
+gem 'unicorn'
+
+group :development do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'annotate'
+end
+
+group :development, :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'autotest'
+  gem 'autotest-rails'
+  gem 'ffaker'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BScCode::Application.config.secret_token = '9c06de2356d6e23cb71b5dbdca707e0682345dada0720ee8457886a65116a844633fbbc4538c469952c946783701837ad8341eacf12cc9850f61c5993b267cde'
