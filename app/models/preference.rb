aa21a0c7a8576928914f63d11ecfd95acc033e1e
kret <andreskee@gmail.com>
2011-07-15 18:45:54 +0200

    Fix :inclusion validation in Preference model.

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -6,10 +6,10 @@ class Preference < ActiveRecord::Base
 
   validates :user,                :presence => true
   validates :book,                :presence => true
-  validates :likes,               :in => [true, false], :allow_nil => true
-  validates :possesses,           :in => [true, false], :allow_nil => true
-  validates :wants_to_read,       :in => [true, false], :allow_nil => true
-  validates :wants_to_give_away,  :in => [true, false], :allow_nil => true
+  validates :likes,               :inclusion => { :in => [true, false] }, :allow_nil => true
+  validates :possesses,           :inclusion => { :in => [true, false] }, :allow_nil => true
+  validates :wants_to_read,       :inclusion => { :in => [true, false] }, :allow_nil => true
+  validates :wants_to_give_away,  :inclusion => { :in => [true, false] }, :allow_nil => true
 
   scope :find_for_book_and_user, lambda { |book, user| where(:user_id => user, :book_id => book) }
 end
